240703975af929ef4fc3d4bf42a7110734e7c401
Payam <payam.kokabi@gmail.com>
2016-08-13 11:17:08 +0430

    WindowsUpdate(2)(23.05.95)

--- app/src/main/java/com/kokabi/p/azmonbaz/Activities/SplashActivity.java
+++ app/src/main/java/com/kokabi/p/azmonbaz/Activities/SplashActivity.java
@@ -20,7 +20,7 @@ public class SplashActivity extends AppCompatActivity {
     CPermissionDeniedDialog dialogPermissionDenied;
 
     /*Activity Values*/
-    int SPLASH_TIME_OUT = 2000;
+    int SPLASH_TIME_OUT = 1000;
 
     @SuppressWarnings("ResultOfMethodCallIgnored")
     @Override
